8961e7ce239f952efb934c0b2dfff3023a2241da
nikhilrajsv054 <70802832+nikhilrajsv054@users.noreply.github.com>
2023-09-27 15:37:01 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import WeekView from "./WeekView";
+
+const App = () => {
+  
+  
+  return (
+    <>
+    <h1 style={{textAlign: "center", padding: "2rem"}}>Habit Tracker</h1>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/week-view" element={<WeekView/>}/>
+    </Routes>
+    </>
+  );
+};
+
+export default App;
